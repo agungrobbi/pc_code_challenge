e8a9001c2a0e98cb35c3fd52551bbd7ba21ec27d
Agung Kurnia Robbi <agung.robbi@gmail.com>
2025-06-26 04:10:34 +0700

    - Remove all auth page and route except for login - Update user seed for admin - Update dashboard and profile route

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::middleware('auth')->name('app.')->group(function () {
+    Route::view('/', 'dashboard')
+        ->middleware(['auth', 'verified'])
+        ->name('dashboard');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+    Route::view('profile', 'profile')
+        ->middleware(['auth'])
+        ->name('profile');
+});
 
 require __DIR__.'/auth.php';
